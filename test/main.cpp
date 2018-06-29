50f830b52def64b5659f76162900fb0aab8f6a14
juliangaal <gjulian@uos.de>
2018-06-30 01:00:48 +0200

    add main for clion building

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,9 @@
 //
-// Created by Julian on 29.06.18.
+// Created by Julian on 30.06.18.
 //
-#include <iostream>
-#include "../include/StopWatch.hpp"
+
+#include "StopWatch.hpp"
 
 int main(void) {
-  StopWatch s;
-  long long int t = 100000000000;
-  double i = 0.0;
-  while (i < t) {
-    i += 100000;
-  }
-  std::cout << s.ElapsedNs() << "ns";
+  return 0;
 }
-
